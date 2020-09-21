8bde45b5ac7b39c6021cf545f6d54d9ffe82e7c2
iwswordpress <iwswordpress@gmail.com>
2020-09-21 10:26:07 +0100

    39

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import DispatchContext from '../DispatchContext';
+import StateContext from '../StateContext';
+
 function HeaderLoggedIn(props) {
   const appDispatch = useContext(DispatchContext);
+  const appState = useContext(StateContext);
   let history = useHistory();
   function handleLogout() {
     appDispatch({ type: 'logout' });
-    localStorage.removeItem('complexappToken');
-    localStorage.removeItem('complexappUsername');
-    localStorage.removeItem('complexappAvatar');
     //redirect HomeGuest
     history.push('/');
   }
@@ -22,10 +22,7 @@ function HeaderLoggedIn(props) {
         <span className='chat-count-badge text-white'> </span>
       </span>
       <a href='#' className='mr-2'>
-        <img
-          className='small-header-avatar'
-          src={localStorage.getItem('complexappAvatar')}
-        />
+        <img className='small-header-avatar' src={appState.user.avatar} />
       </a>
       <Link className='btn btn-sm btn-success mr-2' to='/create-post'>
         Create Post
