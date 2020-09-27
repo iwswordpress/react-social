fa70b7bc62d56d1a440ae97b54ad51dadf557d5a
iwswordpress <iwswordpress@gmail.com>
2020-09-27 11:22:43 +0100

    Add redirect to create post edited error

--- app/Main.js
+++ app/Main.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { useImmerReducer } from 'use-immer';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Axios from 'axios';
-// Axios.defaults.baseURL = 'http://localhost:8080';
+Axios.defaults.baseURL = 'http://localhost:8080';
 
 import StateContext from './StateContext';
 import DispatchContext from './DispatchContext';
@@ -12,13 +12,13 @@ import DispatchContext from './DispatchContext';
 import Header from './components/Header';
 import HomeGuest from './components/HomeGuest';
 import Home from './components/Home';
-import Profile from './components/Profile';
 import Footer from './components/Footer';
 import About from './components/About';
 import Terms from './components/Terms';
 import CreatePost from './components/CreatePost';
 import ViewSinglePost from './components/ViewSinglePost';
 import FlashMessages from './components/FlashMessages';
+import Profile from './components/Profile';
 
 function Main() {
   const initialState = {
@@ -27,9 +27,7 @@ function Main() {
     user: {
       token: localStorage.getItem('complexappToken'),
       username: localStorage.getItem('complexappUsername'),
-      avatar: localStorage.getItem('complexappAvatar'),
-      id: localStorage.getItem('complexappId'),
-      email: localStorage.getItem('complexappEmail')
+      avatar: localStorage.getItem('complexappAvatar')
     }
   };
 
@@ -53,18 +51,15 @@ function Main() {
   useEffect(() => {
     if (state.loggedIn) {
       localStorage.setItem('complexappToken', state.user.token);
-      localStorage.setItem('complexappEmail', state.user.email);
       localStorage.setItem('complexappUsername', state.user.username);
       localStorage.setItem('complexappAvatar', state.user.avatar);
-      localStorage.setItem('complexappId', state.user.id);
     } else {
       localStorage.removeItem('complexappToken');
-      localStorage.removeItem('complexappEmail');
       localStorage.removeItem('complexappUsername');
       localStorage.removeItem('complexappAvatar');
-      localStorage.removeItem('complexappId');
     }
   }, [state.loggedIn]);
+
   return (
     <StateContext.Provider value={state}>
       <DispatchContext.Provider value={dispatch}>
@@ -72,11 +67,11 @@ function Main() {
           <FlashMessages messages={state.flashMessages} />
           <Header />
           <Switch>
-            <Route path='/' exact>
-              {state.loggedIn ? <Home /> : <HomeGuest />}
-            </Route>
             <Route path='/profile/:username'>
               <Profile />
+            </Route>
+            <Route path='/' exact>
+              {state.loggedIn ? <Home /> : <HomeGuest />}
             </Route>
             <Route path='/post/:id'>
               <ViewSinglePost />
