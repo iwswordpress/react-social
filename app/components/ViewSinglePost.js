224cebf5b5a4fab66c9c319093a711905d5190ab
iwswordpress <iwswordpress@gmail.com>
2020-10-01 07:30:50 +0100

    45 MD plus tooltips

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
+import ReactTooltip from 'react-tooltip';
 import Page from './Page';
 import LoadingDotsIcon from './LoadingDotsIcon';
 import StateContext from '../StateContext';
@@ -48,12 +49,23 @@ function ViewSinglePost() {
           {post.id}&nbsp;{post.title}
         </h2>
         <span className='pt-2'>
-          <a href='#' className='text-primary mr-2' title='Edit'>
+          <a
+            href='#'
+            data-tip='Edit'
+            data-for='edit'
+            className='text-primary mr-2'
+          >
             <i className='fas fa-edit'></i>
           </a>
-          <a className='delete-post-button text-danger' title='Delete'>
+          <ReactTooltip id='edit' className='custom-tooltip' />{' '}
+          <a
+            data-tip='Delete'
+            data-for='delete'
+            className='delete-post-button text-danger'
+          >
             <i className='fas fa-trash'></i>
           </a>
+          <ReactTooltip id='delete' className='custom-tooltip' />
         </span>
       </div>
 
